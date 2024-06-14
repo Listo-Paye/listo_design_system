ad570a38bca4b0ae6894c7600bec6826ab53f36a
ThomasS <tsemeria@listopaye.fr>
2024-06-14 16:13:00 +0200

    LIS-1012 - feat - Ajout animation opacité pour le trailing et le leading

--- lib/molecules/listo_navigation_rail.dart
+++ lib/molecules/listo_navigation_rail.dart
@@ -1,16 +1,30 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:listo_design_system/listo_design_system.dart';
-import '../widgets_demo/assets/images/septeo_logo.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+var septeoLogo = SvgPicture.string("""
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="Logo" clip-path="url(#clip0_20_1500)">
+        <path id="Union" fill-rule="evenodd" clip-rule="evenodd"
+              d="M3.27586 17.6686L4.60462 18.9524C4.69119 19.011 4.81238 19.011 4.86864 18.9524L24.3066 0H21.1167L3.27586 17.4093C3.1893 17.4929 3.1893 17.61 3.27586 17.6644V17.6686ZM17.8662 0.0333511L1.64839 15.8744V15.8703C1.59212 15.9246 1.47093 15.9246 1.38437 15.841L0.0556078 14.5571C-0.0309564 14.4735 -0.00498709 14.3606 0.0556078 14.302L14.6763 0.0333511H17.8662ZM1.23291 32L23.8911 9.85262C23.9776 9.79826 24.0945 9.79826 24.1551 9.91117L25.5142 11.195C25.6007 11.2787 25.5748 11.3916 25.5142 11.4501L4.4228 32H1.23291ZM27.1112 12.989L7.64728 31.9958H10.8372L28.7083 14.5865C28.7689 14.5029 28.7689 14.3858 28.7083 14.3314L27.3796 13.0476C27.319 12.9054 27.2021 12.9054 27.1156 12.989H27.1112ZM14.0834 32L30.3315 16.1296V16.1338C30.418 16.0795 30.5392 16.0795 30.6258 16.1631L31.9546 17.4469C32.0151 17.5013 32.0151 17.6184 31.9546 17.702L17.2733 32H14.0834ZM7.79444 22.0932L6.46568 20.8093C6.40509 20.7549 6.40509 20.6379 6.46568 20.5542L27.5268 0.00430021H30.7166L8.05847 22.0932C8.0022 22.1517 7.88101 22.1517 7.79444 22.0932Z"
+              fill="#FF6136" />
+    </g>
+    <defs>
+        <clipPath id="clip0_20_1500">
+            <rect width="32" height="32" fill="white" />
+        </clipPath>
+    </defs>
+  </svg>
+  """);
 
 class DestinationData {
   final IconData icon;
   final IconData selectedIcon;
   final String label;
-  final bool? expendable;
+  final bool expendable;
 
-  DestinationData({required this.icon, required this.selectedIcon, required this.label, this.expendable});
+  DestinationData({required this.icon, required this.selectedIcon, required this.label, required this.expendable});
 }
 
 class ListoNavigationRail extends StatefulWidget {
@@ -28,7 +42,6 @@ class ListoNavigationRailState extends State<ListoNavigationRail> {
   NavigationRailLabelType labelType = NavigationRailLabelType.all;
   bool showLeading = false;
   bool showTrailing = false;
-  bool _variablesExpanded = false;
   double groupAlignment = -1.0;
 
   @override
@@ -42,6 +55,7 @@ class ListoNavigationRailState extends State<ListoNavigationRail> {
           child: SizedBox(
             height: size.height,
             child: NavigationRail(
+              useIndicator: true, // Forme de l'indicateur
               selectedIndex: _selectedIndex,
               destinations: _buildDestinations(),
               extended: extended,
@@ -50,7 +64,7 @@ class ListoNavigationRailState extends State<ListoNavigationRail> {
                   _selectedIndex = index;
                 });
               },
-              leading: MyNavigationRailFab(onPressed: () {
+              leading: NavigationRailLeading(onPressed: () {
                 setState(() {
                   extended = !extended;
                 });
@@ -74,66 +88,73 @@ class ListoNavigationRailState extends State<ListoNavigationRail> {
         icon: Icon(data.icon),
         selectedIcon: Icon(data.selectedIcon),
         label: Text(data.label, style: TextStyles.bodyMedium),
-        padding: const EdgeInsets.symmetric(vertical: Spacings.xs, horizontal: Spacings.xs),
+        padding: const EdgeInsets.symmetric(vertical: Spacings.xs),
       );
     }).toList();
   }
 }
 
-class MyNavigationRailFab extends StatelessWidget {
-  const MyNavigationRailFab({super.key, this.onPressed});
+class NavigationRailLeading extends StatelessWidget {
+  const NavigationRailLeading({super.key, this.onPressed});
 
   final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    final Animation<double> animation =
-    NavigationRail.extendedAnimation(context);
+    final Animation<double> animation = NavigationRail.extendedAnimation(context);
     return AnimatedBuilder(
       animation: animation,
       builder: (BuildContext context, Widget? child) {
         // The extended fab has a shorter height than the regular fab.
         return Container(
-            height: 56,
-            padding: animation.value == 0 ? EdgeInsets.symmetric(
-              vertical: lerpDouble(0, 6, animation.value)!,) : EdgeInsets.zero,
-            child: animation.value == 0 ? MouseRegion(
-                      cursor: SystemMouseCursors.click,
-              child: GestureDetector(
-                  onTap: onPressed,
-                  child: Center(
-                    child: septeoLogo,
-                  ),
-            )
-            ) : Container(
-              margin: const EdgeInsetsDirectional.only(start: Spacings.md, top: 0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Icon(Icons.arrow_back),
-                      ),
-                    ),
-
-                    MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: GestureDetector(
-                        onTap: onPressed,
-                        child: Container(
-                          margin: const EdgeInsetsDirectional.only(
-                              end: 20, top: 0),
-                          child: septeoLogo,
-                        ),
-                      ),
-                    )
-                  ]
+          height: 56,
+          // padding: animation.value == 0
+          //     ? EdgeInsets.symmetric(
+          //   vertical: lerpDouble(0, 6, animation.value)!,
+          // )
+          //     : EdgeInsets.zero,
+          child: animation.value == 0
+              ? MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: onPressed,
+              child: Center(
+                child: septeoLogo,
               ),
-            )
+            ),
+          )
+              : Container(
+            margin: const EdgeInsetsDirectional.only(
+                start: Spacings.md, top: 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: FadeTransition(
+                      opacity: animation,
+                      child: const Icon(Icons.arrow_back),
+                    ),
+                  ),
+                ),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: onPressed,
+                    child: Container(
+                      margin: const EdgeInsetsDirectional.only(
+                          end: 20, top: 0),
+                      child: septeoLogo,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
@@ -156,7 +177,7 @@ class _NavigationRailTrailingState extends State<NavigationRailTrailing> with Si
   void initState() {
     super.initState();
     _opacityController = AnimationController(
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 200),
       vsync: this,
     );
     _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(_opacityController);
@@ -183,7 +204,7 @@ class _NavigationRailTrailingState extends State<NavigationRailTrailing> with Si
         }
 
         return MouseRegion(
-            cursor: SystemMouseCursors.click,
+          cursor: SystemMouseCursors.click,
           child: Padding(
             padding: const EdgeInsets.only(left: Spacings.lg, bottom: Spacings.md),
             child: animation.value < 1 ? const Row(
